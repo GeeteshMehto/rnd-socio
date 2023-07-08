12a6626a3c905884bdec4fc540d12d4c9ac3c254
Geetesh Mehto <87609392+GeeteshMehto@users.noreply.github.com>
2023-07-08 20:12:32 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://sociogram1.onrender.com/users/${_id}/${friendId}`,
+      `https://sociogram1-h4zm.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
