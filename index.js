26bab3da481b015589112cf268a7fd464b0f35a0
Geetesh Mehto <87609392+GeeteshMehto@users.noreply.github.com>
2023-07-09 00:43:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
   origin:["https://sociogram1-h4zm.onrender.com"]
-  ));
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
