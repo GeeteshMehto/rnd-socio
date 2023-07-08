c7a7c30f878380d12be46709b3b44086d7b8ecf7
Geetesh Mehto <87609392+GeeteshMehto@users.noreply.github.com>
2023-07-08 20:08:24 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -35,7 +35,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`https://sociogram1.onrender.com/posts/${postId}/like`, {
+    const response = await fetch(`https://sociogram1-h4zm.onrender.com/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`https://sociogram1.onrender.com/assets/${picturePath}`}
+          src={`https://sociogram1-h4zm.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
