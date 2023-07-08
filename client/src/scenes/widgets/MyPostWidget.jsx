988ba70e5f21ce3077fd2208008674d95b89861b
Geetesh Mehto <87609392+GeeteshMehto@users.noreply.github.com>
2023-07-08 20:09:08 +0530

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`https://sociogram1.onrender.com/posts`, {
+    const response = await fetch(`https://sociogram1-h4zm.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
