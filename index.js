be7fd426216250919db93d0d27ede9f0cf74db52
Geetesh Mehto <87609392+GeeteshMehto@users.noreply.github.com>
2023-07-09 00:38:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:["https://sociogram1-h4zm.onrender.com"]
+  ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
